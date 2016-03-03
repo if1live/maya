2d27bb69e482a795eaadd5c9abb565629c90d50e
if1live <libsora25@gmail.com>
2016-03-03 22:13:09 +0900

    use cache

--- command_execute.go
+++ command_execute.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/op/go-logging"
@@ -23,14 +26,47 @@ func NewCommandExecute(action string, args *CommandArguments) Command {
 	}
 }
 
+func (c *CommandExecute) CacheFileName() string {
+	// 실행 경로를 캐시 생성 경로로 이용
+	pwd, _ := os.Getwd()
+
+	cachePath := filepath.Join(pwd, "cache")
+	os.MkdirAll(cachePath, 0755)
+
+	data := []byte(c.Cmd)
+	filename := fmt.Sprintf("%x.txt", md5.Sum(data))
+
+	cacheFile := filepath.Join(cachePath, filename)
+	return cacheFile
+}
+
 func (c *CommandExecute) RawOutput() []string {
-	log := logging.MustGetLogger("maya")
-	log.Infof("Command execute: %v", c)
+	cacheFilePath := c.CacheFileName()
+	_, err := os.Stat(cacheFilePath)
+
+	outputLines := []string{}
+	if os.IsNotExist(err) {
+		outputLines = c.ExecuteImmediately()
+		data := []byte(strings.Join(outputLines, "\n"))
+		ioutil.WriteFile(cacheFilePath, data, 0644)
+
+	} else {
+		data, _ := ioutil.ReadFile(cacheFilePath)
+		text := string(data[:])
+		outputLines = strings.Split(text, "\n")
+	}
 
 	elems := []string{}
 	if c.AttachCmd {
 		elems = append(elems, "$ "+c.Cmd)
 	}
+	elems = append(elems, outputLines...)
+	return elems
+}
+
+func (c *CommandExecute) ExecuteImmediately() []string {
+	log := logging.MustGetLogger("maya")
+	log.Infof("Command execute: %v", c)
 
 	tmpfile, err := ioutil.TempFile("", "maya")
 	if err != nil {
@@ -46,6 +82,7 @@ func (c *CommandExecute) RawOutput() []string {
 	}
 	out, err := exec.Command("bash", tmpfile.Name()).CombinedOutput()
 
+	elems := []string{}
 	if err != nil {
 		if _, ok := err.(*exec.Error); ok {
 			elems = append(elems, err.Error())
@@ -53,7 +90,7 @@ func (c *CommandExecute) RawOutput() []string {
 		}
 	}
 
-	elems = append(elems, strings.Split(string(out[:]), "\n")...)
+	elems = strings.Split(string(out[:]), "\n")
 	return elems
 }
 
